eba489931aac7833d6eba9770316fa08dca050fd
minseok <dhkdwk016@gmail.com>
2025-10-17 22:32:54 +0000

    Updates

---
+++ client/assets/about-gUqlecP8.js
@@ -0,0 +1,1 @@
+import{w as o,p as t}from"./chunk-OIYGIGL5-Cu3wia-V.js";const s=o(function(){return t.jsx(t.Fragment,{children:"About..."})});export{s as default};
---
+++ client/assets/contact-BW_QelFA.js
@@ -0,0 +1,1 @@
+import{w as n,p as t}from"./chunk-OIYGIGL5-Cu3wia-V.js";const a=n(function(){return t.jsx(t.Fragment,{children:"Contact..."})});export{a as default};
