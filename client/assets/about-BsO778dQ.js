471f8e5fff18f0c73c05add3c76099f514e5abd0
minseok <dhkdwk016@gmail.com>
2025-10-18 12:49:20 +0000

    Updates

---
+++ client/assets/about-BsO778dQ.js
@@ -0,0 +1,1 @@
+import{w as o,p as t}from"./chunk-OIYGIGL5-rbcUeYSU.js";const s=o(function(){return t.jsx(t.Fragment,{children:"About..."})});export{s as default};
---
+++ client/assets/resume-Lr7FlqOr.js
@@ -0,0 +1,1 @@
+import{w as s,p as e}from"./chunk-OIYGIGL5-rbcUeYSU.js";const n=s(function(){return e.jsx(e.Fragment,{children:"Resume..."})});export{n as default};
