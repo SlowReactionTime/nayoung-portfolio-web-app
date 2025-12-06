7ee4722154d006d1de0b122ef931fea3f0034ca8
minseok <dhkdwk016@gmail.com>
2025-12-06 00:46:00 +0000

    feat: edit home page description

--- app/routes/home.tsx
+++ app/routes/home.tsx
@@ -1,7 +1,7 @@
 import type { Route } from "./+types/home";
 import { Button, Container, Group, Image, List, Text, ThemeIcon, Title } from '@mantine/core';
 import classes from './home.module.css';
-import image from '../assets/nayoungPic.jpeg';
+import image from '../assets/portfolio-whisper-of-the-enchanted-world.png';
 import { useNavigate } from 'react-router-dom';
 
 export function meta({}: Route.MetaArgs) {
@@ -14,17 +14,14 @@ export function meta({}: Route.MetaArgs) {
 export default function Home() {
   const navigate = useNavigate();
   return (
-    <Container size="md">
+    <Container size="lg">
       <div className={classes.inner}>
         <div className={classes.content}>
           <Title className={classes.title}>
             Nayoung <span className={classes.highlight}>"Luna"</span> Kim <br />
           </Title>
           <Text c="dimmed" mt="lg">
-            I'm sleeping. Better not wake me up!!! I like snacks, desserts, cute clothes, and pigs.
-            I like Olaf the happy snowman and Kuromi the the bringer of the end times.
-            I don't like princesses because I am the best of them all :)
-            Anyways, you should hire me because:
+            Nayoung “Luna" Kim is a 3D and VFX artist based in Chicago. She creates immersive visuals that blend storytelling with cutting-edge technology.
           </Text>
 
           <List
@@ -39,7 +36,7 @@ export default function Home() {
               <b>I love to</b> : model and craft animations that capture the imagination.
             </List.Item>
             <List.Item className={classes.listItem}>
-              <b>I am literally the best</b> : don't take my word for it! My brother tells me so, and he's never wrong.
+              <b>I create</b> : works that challenge viewers to reconsider the everyday and view it from a new, often transformative perspective.
             </List.Item>
           </List>
 
